94eda448ecd8a2ce603a589eab8dfab75e72260a
usamashafique <usama_shafiq97@hotmail.com>
2022-09-28 12:36:03 +0500

    Trading view Chart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import TradingViewWidget, { Themes } from "react-tradingview-widget";
 
 function App() {
+  const [value, setValue] = useState("BTCUSDT");
+  const [exchange, setExchange] = useState("Binance");
+
+  const exchanges = [
+    {
+      label: "Binance"
+    },
+    {
+      label: "FTX"
+    }
+  ]
+  const options = [
+    {
+      label: "BTCUSDT",
+      value: "BTCUSDT",
+    },
+    {
+      label: "DOTUSDT",
+      value: "DOTUSDT",
+    },
+    {
+      label: 'LTCBTC',
+      value: 'LTC/BTC'
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <select id="exchange" onChange={(e) => setExchange(e.target.value)}>
+      {exchanges.map((exchange)=>(
+        <option value={exchange.label}>{exchange.label}</option>
+      ))}
+      </select>
+      <select id="pairs" onChange={(e) => setValue(e.target.value)}>
+        {options.map((option) => (
+          <option value={option.value}>{option.label}</option>
+        ))}
+      </select>
+      <br />
+      <div class="tradingview-widget-container" id="tradingbot-chart">
+        {/* <div className="App"> */}
+        <TradingViewWidget
+          width={`1300px`}
+          height={`850px`}
+          symbol={`${exchange}:${value}`}
+          theme={Themes.DARK}
+          locale="en"
+          // timezone="America/New York"
+          hideSideToolbar={true}
+          details
+          news={["headlines"]}
+          hide_side_toolbar={false}
+        />
+      </div>
+    </body>
   );
 }
 
