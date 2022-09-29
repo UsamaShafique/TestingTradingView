5d4349357b3df5bfb27277e5543c814824b20e26
Umair Javed <112403066+imumairjaved@users.noreply.github.com>
2022-09-29 11:51:32 +0500

    api calls for binances (#1)
    Co-authored-by: Umair <umair.javed@codora.io>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TradingViewWidget, { Themes } from "react-tradingview-widget";
+
+
 
 function App() {
   const [value, setValue] = useState("BTCUSDT");
   const [exchange, setExchange] = useState("Binance");
+  const [pairs, setPairs] = useState([]);
+
 
   const exchanges = [
     {
@@ -14,31 +18,49 @@ function App() {
       label: "FTX"
     }
   ]
-  const options = [
-    {
-      label: "BTCUSDT",
-      value: "BTCUSDT",
-    },
-    {
-      label: "DOTUSDT",
-      value: "DOTUSDT",
-    },
-    {
-      label: 'LTCBTC',
-      value: 'LTC/BTC'
+  const options = ['BTCUSDT', 'DOTUSDT', 'LTC/BTC'];
+
+
+  useEffect(() => {
+    getExchangeData('Binance');
+  }, []);
+
+  var pairsArray = [];
+
+  const getExchangeData = async (e) => {
+    let response;
+    switch (e) {
+      case 'Binance':
+        response = await fetch(
+          "https://api.binance.com/api/v3/exchangeInfo"
+        ).then((response) => response.json());
+        for (let j = 0; j < response.symbols.length; j++) {
+          pairsArray.push(
+            response.symbols[j].symbol
+          );
+        }
+        setPairs(pairsArray);
+        break;
+      case 'FTX':
+        setPairs(options);
+        break;
+      default:
+        setPairs([]);
+        break;
     }
-  ];
+  };
+
 
   return (
     <body>
-      <select id="exchange" onChange={(e) => setExchange(e.target.value)}>
-      {exchanges.map((exchange)=>(
-        <option value={exchange.label}>{exchange.label}</option>
-      ))}
+      <select id="exchange" onChange={(e) => { setExchange(e.target.value); getExchangeData(e.target.value) }}>
+        {exchanges.map((exchange) => (
+          <option value={exchange.label}>{exchange.label}</option>
+        ))}
       </select>
       <select id="pairs" onChange={(e) => setValue(e.target.value)}>
-        {options.map((option) => (
-          <option value={option.value}>{option.label}</option>
+        {pairs.map((option, i) => (
+          <option key={i} value={option}>{option}</option>
         ))}
       </select>
       <br />
@@ -50,12 +72,16 @@ function App() {
           symbol={`${exchange}:${value}`}
           theme={Themes.DARK}
           locale="en"
+          // hide_top_toolbar = {false}
           // timezone="America/New York"
           hideSideToolbar={true}
           details
           news={["headlines"]}
           hide_side_toolbar={false}
         />
+
+        <br />
+        <br />
       </div>
     </body>
   );
